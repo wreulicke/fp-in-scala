64ac7348ea14ad7a26babb3ffa766a77e06beefb
wreulicke <saito.masaya@classmethod.jp>
2017-12-10 12:43:23 +0900

    init

---
+++ src/test/scala/com/github/wreulicke/List.scala
@@ -0,0 +1,156 @@
+package com.github.wreulicke
+
+import com.github.wreulicke.List.foldLeft
+
+import scala.annotation.tailrec
+
+sealed trait List[+A] {
+  def tail(): List[A]
+  def setHead[B >: A](a: B): List[B]
+}
+case object Nil extends List[Nothing] {
+  override def tail(): List[Nothing] = Nil
+
+  override def setHead[B >: Nothing](a: B): List[B] = List(a)
+}
+case class Cons[+A](head: A, tail:List[A]) extends List[A] {
+  override def setHead[B >: A](a: B): List[B] = Cons(a, tail)
+}
+
+object List{
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+
+  def product(ds:List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  @tailrec
+  def drop[A](l: List[A], n:Int): List[A] = {
+    n match {
+      case _ if n < 0 => ???
+      case 0 => l
+      case _ =>
+        l match {
+          case Nil => Nil
+          case Cons(_, xs) => drop(xs, n - 1)
+        }
+    }
+  }
+
+  @tailrec
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(x, xs) =>
+        if (f(x)) dropWhile(xs, f)
+        else l
+    }
+  }
+
+  def init[A](l: List[A]): List[A] = {
+    l match {
+      case Nil => Nil
+      case Cons(_, Nil) => Nil
+      case Cons(x, xs) => Cons(x, init(xs))
+    }
+  }
+
+  def apply[A](as: A*): List[A] = {
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+  }
+
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  }
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  def length[A](as: List[A]):Int = foldRight(as, 0)((_, result) => result + 1)
+
+  // exercise 3.11
+  def sum3(ns: List[Int]):Int = foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]): Double = foldLeft(ns, 1.0)(_ * _)
+
+  def length2[A](as: List[A]):Int = foldLeft(as, 0)((result, _) => result + 1)
+
+  def empty[A](): List[A] = Nil
+
+  def reverse[A](as: List[A]) =
+    foldLeft(as, Nil:List[A])((result, x) => Cons(x, result))
+
+  def foldRight2[A, B](as: List[A], z: B)(f: (A, B) => B): B = {
+    foldLeft(as, (b:B) => b)((g, a) => b => g(f(a, b)))(z)
+  }
+
+  def append[A](as: List[A], as2:List[A]) :List[A] = foldRight(as, as2)((x, r) => Cons(x, r))
+
+  def append2[A](as: List[A], as2:List[A]) :List[A] = foldLeft(reverse(as), as2)((b, a) => Cons(a, b))
+
+  def flatten[A](list: List[List[A]]) = foldLeft(list, Nil:List[A])((b, a) => append(b, a))
+
+  // exercise 3.16
+  def add1(ns: List[Int]): List[Int] = {
+    ns match {
+      case Nil => Nil
+      case Cons(x, xs) => Cons(x + 1, add1(xs))
+    }
+  }
+
+  def add1_take2(ns: List[Int]): List[Int] =
+    foldLeft(ns, Nil:List[Int]) {
+      (b, a) => Cons(a + 1, b)
+    }
+    // foldRight(ns, Nil:List[Int])((a, b) => Cons(a + 1, b))
+
+  // exercise 3.17
+  def strings(ds: List[Double]): List[String] =
+    foldLeft(ds, Nil:List[String]) {
+      (b, a) => Cons(a.toString, b)
+    }
+
+  // exercise 3.18
+
+  def map[A, B](as: List[A])(f: A => B): List[B] =
+    foldLeft(as, Nil:List[B])((b, a) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = foldLeft(as, Nil:List[A]) {
+    (b, a) =>
+      if (f(a)) {
+        Cons(a, b)
+      } else {
+        b
+      }
+  }
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    foldLeft(as, Nil:List[B]) {
+      (b, a) => append(b, f(a))
+    }
+  }
+
+  def filterViaFlatMap[A](as: List[A])(f: A => Boolean): List[A] = flatMap(as)(a =>
+    if(f(a)) List(a)
+    else Nil
+  )
+
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+  }
+
+}
---
+++ src/test/scala/com/github/wreulicke/ListTest.scala
@@ -0,0 +1,139 @@
+package com.github.wreulicke
+
+import org.scalatest.FreeSpec
+
+class ListTest extends FreeSpec {
+
+  "Exercise 3-1" - {
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(x, Cons(2, Cons(4, _))) => x
+      case Nil => 42
+      case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+      case Cons(h, t) => h + List.sum(t)
+    }
+    assert(x == 3)
+  }
+
+  "Ex 3-2 List.tail" - {
+    "Cons(x, Nil).tail be Nil" in {
+      assert(List(1).tail == Nil)
+    }
+
+    "Cons(x, Cons(...)).tail be Cons(...)" in {
+      assert(List(1, 2, 3).tail == List(2, 3))
+    }
+
+    "Nil.tail be Nil" in {
+      assert(List(1, 2, 3).tail == List(2, 3))
+    }
+  }
+
+  "Ex 3-3 List.setHead" - {
+    "Nil.setHead be Cons" in {
+      assert(Nil.setHead(2) == List(2))
+    }
+
+    "Cons(x, xs).setHead(a) be Cons(a, xs)" in {
+      assert(List(1, 2, 3).setHead(2) == List(2, 2, 3))
+    }
+  }
+
+  "Ex 3-4 List.drop" - {
+    "drop(Nil, 1) => Nil" in {
+      assert(List.drop(Nil, 1) == Nil)
+    }
+    "drop(List(1), 0) => List(1)" in {
+      assert(List.drop(List(1), 0) == List(1))
+    }
+    "drop(List(1, 2, 3), 1) => List(2, 3)" in {
+      assert(List.drop(List(1, 2, 3), 1) == List(2, 3))
+    }
+    "drop(_, negative) => thrown ???" in {
+      assertThrows[NotImplementedError](List.drop(Nil, -1))
+    }
+  }
+
+  "Ex 3-5 List.dropWhile" - {
+    "dropWhile(Nil, _ => true) => Nil" in {
+      assert(List.dropWhile(Nil, (_: Nothing) => true) == Nil)
+    }
+    "dropWhile(List(1, 2, 3), n => 1) => List(2, 3)" in {
+      assert(List.dropWhile(List(1, 2, 3), (_: Int) == 1) == List(2, 3))
+    }
+    "dropWhile(List(1, 2, 3), _ => false) => List(1, 2, 3)" in {
+      assert(List.dropWhile(List(1, 2, 3), (_: Int) => false) == List(1, 2, 3))
+    }
+  }
+
+  "Ex 3-6 List.init" - {
+    "init(Nil, _ => true) => Nil" in {
+      assert(List.init(Nil) == Nil)
+    }
+    "init(List(1, 2, 3)) => List(2, 3)" in {
+      assert(List.init(List(1, 2, 3)) == List(1, 2))
+    }
+  }
+
+  "Ex 3-10 List.foldLeft" - {
+    "Nil" in {
+      assert(List.foldLeft(Nil: List[Int], 2)(_ + _) == 2)
+    }
+    "Cons" in {
+      assert(List.foldLeft(Cons(3, Nil), 2)(_ + _) == 5)
+    }
+  }
+
+  "Ex 3-11 sum, product, length" - {
+    "sum" in {
+      assert(List.sum3(Cons(1, Nil)) == 1)
+      assert(List.sum3(Cons(3, Cons(2, Nil))) == 5)
+      assert(List.sum3(Nil) == 0)
+    }
+    "product" in {
+      assert(List.product3(Cons(1.1, Nil)) == 1.1)
+      assert(List.product3(Cons(2.0, Cons(1.1, Nil))) == 2.2)
+      assert(List.product3(Nil) == 1.0)
+    }
+    "length" in {
+      val list1 = Cons(1, Nil)
+      val list2 = Cons(2, list1)
+      val list3 = Cons(3, list2)
+      assert(List.length2(list1) == 1)
+      assert(List.length2(list2) == 2)
+      assert(List.length2(list3) == 3)
+    }
+  }
+
+  "Ex 3-12 List.reverse" - {
+    "Nil" in {
+      assert(List.reverse(List(1, 2)) == List(2, 1))
+    }
+  }
+
+  "Ex 3-15" - {
+    "flatten" in {
+      assert(List.flatten(List(List(1, 2), List(3, 4))) == List(1, 2, 3, 4))
+    }
+  }
+
+  "Ex 3-16" - {
+    "append2" in {
+      assert(List.append2(List(1, 2), List(3, 4)) == List(1, 2, 3, 4))
+    }
+  }
+
+  "Ex 3-20" - {
+    "flatMap" in {
+      assert(List.flatMap(List(1, 2, 3))(i => List(i, i)) == List(1, 1, 2, 2, 3, 3))
+    }
+  }
+
+  "Ex 3-21" - {
+    "filter" in {
+      assert(List.filter(List(1, 2, 3))(_ == 2) == List(2))
+    }
+    "filterViaFlatMap" in {
+      assert(List.filterViaFlatMap(List(1, 2, 3))(_ == 2) == List(2))
+    }
+  }
+}
